cb8fae99bfb5447e04784538e31b1ea612c7b0ae
aniketdindefynd <aniketdinde@gofynd.com>
2025-09-05 19:10:16 +0530

    added eslint, prettier and workflow

---
+++ server/eslint.config.js
@@ -0,0 +1,9 @@
+ export default [
+    {
+        files: ["**/*.js"], //Only check files with .js extension 
+        rules: {
+            semi: "error",
+            "no-unused-vars": "warn",
+        }
+    }
+];
